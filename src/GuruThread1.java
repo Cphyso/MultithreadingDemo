ab486774b9c2e8ccd62520648ead8a14277669b2
Mhlongo <Sifiso.Mhlongo@standardbank.co.za>
2021-05-27 22:54:21 +0200

    second commit

--- src/GuruThread1.java
+++ src/GuruThread1.java
@@ -4,7 +4,7 @@ public class GuruThread1 implements Runnable
         Thread guruThread1 = new Thread("Guru1");
         Thread guruThread2 = new Thread("Guru2");
         guruThread1.start();
-        guruThread2.start();
+        
         System.out.println("Thread names are following:");
         System.out.println(guruThread1.getName());
         System.out.println(guruThread2.getName());
